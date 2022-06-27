188e408f734eb55c5f25f7ef1514fdd6e93a2bd5
Gianluca O <17207260+RandomGgames@users.noreply.github.com>
2022-06-27 08:28:02 -0400

    Update Updater.py
    Rewritten from the ground up
    No longer automatically updates fabric
    Now supports modrinth links
    Started groundwork for forge mod support

--- Updater.py
+++ Updater.py
@@ -1,177 +1,203 @@
-import json
-import requests
-import os
+from ast import excepthandler
 import datetime
-from zipfile import ZipFile
+import json
+import os
+import requests
 import shutil
+from zipfile import ZipFile
 
 run_time = datetime.datetime.now().strftime("%Y-%m-%d")
-if os.path.exists('mod_manager_logs/latest.log'): open('mod_manager_logs/latest.log', 'w').close()
-def log(text):
+if os.path.exists("mod_manager_logs/latest.log"): open("mod_manager_logs/latest.log", "w").close()
+def log(text, end = "\n"):
 	"""
 	Prints text to the console and appends date, time, and text to a logs.txt file text. class str. The text to log
 	"""
-	if not os.path.exists('mod_manager_logs'): os.makedirs('mod_manager_logs')
-	print(str(text))
+	if not os.path.exists("mod_manager_logs"): os.makedirs("mod_manager_logs")
+	print(str(text), end = end)
 	with open(f"mod_manager_logs/{run_time}.log", "a", encoding="UTF-8") as file: file.write(f"{str(text)}\n")
 	with open(f"mod_manager_logs/latest.log", "a", encoding="UTF-8") as file: file.write(f"{str(text)}\n")
 
-def logExit(text):
-	log(text)
+def logExit(text, end = "\n"):
+	log(text, end = end)
 	exit()
-
-mods_downloaded = 0
-mods_updated = 0
-class CurseforgeMod:
-	def __init__(self, slug, version):
-		data = requests.get('https://api.curseforge.com/v1/mods/search', params = {'gameId': '432','slug': slug, 'classId': '6'}, headers = headers).json()['data']
-		try: self.data = data[0]
-		except: logExit(f"			[WARN] FATAL: Could not find mod \"https://www.curseforge.com/minecraft/mc-mods/{slug}\". Please make sure the URL is still valid.")
-		self.id = self.data['id']
-		self.slug = slug
-		self.url = self.data['links']['websiteUrl']
-		self.version = version
-		
-		files = requests.get(f"https://api.curseforge.com/v1/mods/{self.id}/files", params = {'gameVersion': self.version, 'modLoaderType': 4}, headers = headers).json()['data']
-		if len(files) != 0 : self.files = files
-		else: self.files = None
-
-		if self.files:
-			download_url = requests.get(f"https://api.curseforge.com/v1/mods/{self.id}/files/{self.files[0]['id']}/download-url", headers = headers)
-			if download_url.content == b"": self.download_url = f"https://edge.forgecdn.net/files/{str(self.files[0]['id'])[0:4]}/{str(self.files[0]['id'])[4:7]}/{self.files[0]['fileName']}"
-			else: self.download_url = download_url.json()['data']
-		else:
-			self.download_url = None
-			log(f"			[WARN] Cannot find {self.version} file for {self.url}")
-
-	def __str__(self):
-		return {'data': self.data, 'id': self.id, 'slug': self.slug, 'url': self.url, 'files': self.files, 'download_url': self.download_url}
-
-	def downloadLatestFile(self, download_location, copy_locations):
-		if self.download_url:
-			file_name = self.files[0]['fileName']
-			if not os.path.exists(f"{download_location}/{file_name}"):
-				try:
-					open(f"{download_location}/{file_name}", 'wb').write(requests.get(self.download_url).content)
-					global mods_downloaded
-					mods_downloaded += 1
-					log(f"			[INFO] Downloaded {self.version} mod \"{file_name}\"")
-				except Exception as e: log(f"			[WARN] Error downloading {self.url}. {e}")
-			else: log(f"			Already up to date.")
-			for copy_location in copy_locations:
-				if not os.path.exists(f"{copy_location}/{file_name}"):
-					try:
-						shutil.copyfile(f"{download_location}/{file_name}", f"{copy_location}/{file_name}")
-						global mods_updated
-						mods_updated += 1
-						log(f"			[INFO] Copied \"{download_location}/{file_name}\" to \"{copy_location}\"")
-					except Exception as e: log(f"			[WARN] Could not copy mod \"{download_location}/{file_name}\" to \"{copy_location}\". {e}")
-		else: return None
 
 
 log(f"[{datetime.datetime.now()}] RUNNING MOD UPDATER")
 
-
-"""LOAD CONFIG"""
-log("[INFO] LOADING CONFIG")
+"""
+LOAD CONFIG
+"""
+log("[INFO] LOADING CONFIG:")
 try:
-	with open('Manager Config.json') as f: config = json.load(f)
-	log('	Config has been read')
+	with open("Manager Config.json") as f:
+		config = json.load(f)
+		download_mods_location = config["download_mods_location"]
+		instances = config["instances"]
+		headers = {"Accept": "application/json", "x-api-key": config["x-api-key"]}
+		log("	Config read successfully.")
 except Exception as e:
-	logExit(f'	[WARN] FATAL: Loading config file. {e}')
-if len(config['x-api-key']) != 60 or config['x-api-key'][:7] != '$2a$10$':
-	logExit("	[WARN] FATAL: x-api-key doesn't look valid. Please provide a valid API key.")
-headers = {'Accept': 'application/json', 'x-api-key': config['x-api-key']}
+	logExit(f"	[WARN] Could not read config file. Please fix the following error: {repr(e)}")
 
-HAD_ERROR = False
-for instance in config['instances']:
-	if not os.path.isdir(instance['directory']):
-		HAD_ERROR = True
-		log(f"	[WARN] Location \"{instance['directory']}\" is invalid/doesn't exist!")
-	else:
-		log(f"	Found {instance['directory']}")
-if HAD_ERROR:
-	logExit('	[WARN] FATAL: Cannot continue until directories listed above can be found.')
+if not os.path.exists(download_mods_location):
+	os.makedirs(download_mods_location)
+	log(f"	[INFO] Created folder {download_mods_location}")
 
-organized_config = {}
-for instance in config['instances']:
-	version = instance['version']
-	directory = instance['directory']
-	
-	if version not in organized_config:
-		organized_config[version] = {}
-	
-	for mod_link in instance['mod_links']:
-		if mod_link[0:45] == 'https://www.curseforge.com/minecraft/mc-mods/':
-			if mod_link[-1] != '/':
-				slug = mod_link[45:]
-				if '/' not in slug or ' ' not in slug or '\\' not in slug:
-					if slug not in organized_config[version]:
-						organized_config[version][slug] = {'directories': []}
-					organized_config[version][slug]['directories'].append(directory)
-				else: log(f'	[WARN] Invlalid slug: "{slug}"')
-			else: log(f'	[WARN] Remove trailing "/" at the end of the url! "{mod_link}"')
-		else: log(f'	[WARN] "{mod_link}" is not a curseforge mod and thus cannot be auto-downloaded.')
-
-for version in organized_config:
-	try:
-		if not os.path.exists(f"{config['download_mods_location']}/{version}"): os.makedirs(f"{config['download_mods_location']}/{version}")
-	except Exception as e:
-		logExit('	[WARN]	FATAL: Could not generate folders to download mods into.')
-
-
-"""UPDATING FABRIC"""
-log("[INFO] UPDATING FABRIC")
-
-installer_version = requests.get(f"https://meta.fabricmc.net/v2/versions/installer").json()[0]['version']
-if not os.path.exists(f"{config['download_mods_location']}/fabric-installer-{installer_version}.jar"):
-	open(f"{config['download_mods_location']}/fabric-installer-{installer_version}.jar", 'wb').write(requests.get(f"https://maven.fabricmc.net/net/fabricmc/fabric-installer/{installer_version}/fabric-installer-{installer_version}.jar").content)
-	log(f"	[INFO] Downloaded {config['download_mods_location']}/fabric-installer-{installer_version}.jar")
-else:
-	log(f"	Installer already up to date")
-
-fabric_version = requests.get(f"https://meta.fabricmc.net/v2/versions/loader").json()[0]['version']
-for version in organized_config:
-	if f"fabric-loader-{fabric_version}-{version}" not in os.listdir(f"{os.getenv('APPDATA')}/.minecraft/versions"):
-		log(f"	Need to install fabric-loader-{fabric_version}-{version}")
-	else:
-		log(f"	Fabric {version} is up to date")
+log(f"	Done")
 
 
 """UPDATING MODS"""
-log("[INFO] UPDATING MODS")
-for version in organized_config:
-	log(f"	Processing {version} mods:")
-	for slug in organized_config[version]:
-		log(f"		Scanning {slug}")
-		mod = CurseforgeMod(slug, version)
-		mod.downloadLatestFile(f"{config['download_mods_location']}/{version}", organized_config[version][slug]['directories'])
+log("[INFO] PROCESSING LIST OF MODS:")
+cache = {}
+for instance_index, instance in enumerate(instances):
+	instance_text = instance_index + 1
+	try:
+		loader = instance['loader'].capitalize()
+		version = instance['version']
+		mods_directory = f"{instance['directory']}/mods"
+		links = instance['mod_links']	
+	except Exception as e:
+		logExit(f"	[WARN] Instance {instance_text} is missing the key: {repr(e)}")
+	
+	#for dir in [mods_directory, resource_packs_directory, shaderpacks_directory]:
+	#	if not os.path.exists(dir):
+	#		log(f"	[WARN]: Could not find dir \"{dir}\".")
+	
 
-if mods_downloaded == 1: log(f"	Downloaded {mods_downloaded} mod.")
-else: log(f"	Downloaded {mods_downloaded} mods.")
+	if not os.path.exists(f"{download_mods_location}/{loader}/{version}"):
+		try:
+			os.makedirs(f"{download_mods_location}/{loader}/{version}")
+			log(f"	[INFO] Created folder {download_mods_location}/{loader}/{version}")
+		except Exception as e:
+			log(f"	[WARN] Could not create {download_mods_location}/{loader}/{version}. {repr(e)}")
 
-if mods_updated == 1: log(f"	Updated {mods_updated} mod.")
-else: log(f"	Updated {mods_updated} mods.")
+	
+	if loader == "Fabric":
+		curseforge_modLoaderType = 4
+		log(f"	Updating {version} mods in {mods_directory}...")
+
+		for link in links:
+			if link[-1] == "/": link = link[:-1] #Remove trailing / from link sif it has one at the end
+			
+			if link[0:45] == "https://www.curseforge.com/minecraft/mc-mods/":
+				if link[45:len(link)] != "":
+					slug = link[45:len(link)]
+					if not any(x in slug for x in ["/", " ", "\\"]):
+						log(f"		Updating: {slug}")
+
+						if link not in cache:
+							log(f"			Caching {slug} for {version}...")
+							cache[link] = {"versions": {}}
+
+							curseforge_mod = requests.get("https://api.curseforge.com/v1/mods/search", params = {"gameId": "432","slug": slug, "classId": "6"}, headers = headers).json()["data"]
+							if len(curseforge_mod) > 0:
+								curseforge_id = curseforge_mod[0]["id"]
+							
+								curseforge_files = requests.get(f"https://api.curseforge.com/v1/mods/{curseforge_id}/files", params = {"gameVersion": version, "modLoaderType": curseforge_modLoaderType}, headers = headers).json()["data"]
+								if len(curseforge_files) > 0:
+									latest_curseforge_file = curseforge_files[0]
+									file_name = latest_curseforge_file["fileName"]
+									download_url = latest_curseforge_file["downloadUrl"]
+									if download_url == "": download_url = f"https://edge.forgecdn.net/files/{latest_curseforge_file['id'][0:4]}/{latest_curseforge_file['id'][4:7]}/{file_name}"
+
+									if not os.path.exists(f"{download_mods_location}/{loader}/{version}/{file_name}"):
+										try:
+											open(f"{download_mods_location}/{loader}/{version}/{file_name}", "wb").write(requests.get(download_url).content)
+											log(f"			[INFO] Downloaded {file_name} to {download_mods_location}/{loader}/{version}")
+										except Exception as e:
+											log(f"			[WARN] Could not download file. {repr(e)}")
+
+									cache[link]["versions"][version] = f"{download_mods_location}/{loader}/{version}/{file_name}"
+								else: log(f"			[WARN] Cannot find any {version} compatable versions of this mod.")
+
+							else:
+								log(f"		[WARN] Could not find mod {slug}. Make sure the url {link} is valid and not a redirect!")
+
+						if link in cache:
+							if not os.path.exists(f"{mods_directory}/{os.path.basename(cache[link]['versions'][version])}"):
+								try:
+									shutil.copyfile(cache[link]["versions"][version], f"{mods_directory}/{os.path.basename(cache[link]['versions'][version])}")
+									log(f"			[INFO] Copied \"{cache[link]['versions'][version]}\" into \"{mods_directory}\"")
+								except Exception as e:
+									log(f"			[WARN] Something went wrong copying \"{cache[link]['versions'][version]}\" into \"{mods_directory}\". {repr(e)}")
+							else: log(f"			{slug} is already up to date.")
+					
+					else: log(f'		[WARN] Invlalid slug: "{slug}"')
+				
+				else:  log(f"	[WARN] Links must be to a mod page. {link} is not a valid mod page link.")
+
+			elif link[0:25] == "https://modrinth.com/mod/":
+				if link[25:len(link)] != "":
+					slug = link[25:len(link)]
+					if not any(x in slug for x in ["/", " ", "\\"]):
+						log(f"		Updating: {slug}")
+						
+						if link not in cache:
+							log(f"			Caching {slug} for {version}...")
+							cache[link] = {"versions": {}}
+							
+							modrinth_files = requests.get(f'https://api.modrinth.com/v2/project/{slug}/version?game_versions=["{version}"]').json()
+							if len(modrinth_files) > 0:
+								latest_version = modrinth_files[0]
+								
+								project_id = latest_version['project_id']
+								mod_version = latest_version['version_number']
+								file_name = latest_version['files'][0]['filename']
+								download_url = f'https://cdn.modrinth.com/data/{project_id}/versions/{mod_version}/{file_name}'
+							
+								if not os.path.exists(f'{download_mods_location}/{loader}/{version}/{file_name}'):
+									try:
+										open(f"{download_mods_location}/{loader}/{version}/{file_name}", "wb").write(requests.get(download_url).content)
+										log(f"			[INFO] Downloaded {file_name} to {download_mods_location}/{loader}/{version}")
+									except Exception as e:
+										log(f"			[WARN] Could not download file. {repr(e)}")
+
+								cache[link]["versions"][version] = f"{download_mods_location}/{loader}/{version}/{file_name}"
+							
+							else:
+								log(f"			[WARN] Could not find mod {slug}. Make sure the url {link} is valid and not a redirect!")
+
+						if link in cache:
+							if not os.path.exists(f"{mods_directory}/{os.path.basename(cache[link]['versions'][version])}"):
+								try:
+									shutil.copyfile(cache[link]["versions"][version], f"{mods_directory}/{os.path.basename(cache[link]['versions'][version])}")
+									log(f"			[INFO] Copied \"{cache[link]['versions'][version]}\" into \"{mods_directory}\"")
+								except Exception as e:
+									log(f"			[WARN] Something went wrong copying \"{cache[link]['versions'][version]}\" into \"{mods_directory}\". {repr(e)}")
+							else: log(f"			{slug} is already up to date.")	
+					
+					else: log(f'		[WARN] Invlalid slug: "{slug}"')
+				
+				else:  log(f"	[WARN] Links must be to a mod page. {link} is not a valid mod page link.")
+
+
+	elif loader == "Forge":
+		curseforge_modLoaderType = 1
+		log(f"	[WARN] Script does not currently support {loader} mods. Ignoring instance {instance_text}.")
+		
+	else:
+		log(f"	[WARN] Ignoring instance {instance_text}. Does not support {loader} loader.")
+	
+log(f"	Done")
 
 
 """DELETE OLD MODS"""
 log("[INFO] DELETING OLD MODS")
-versions = []
-for instance in config['instances']:
-	version = instance['version']
-	if version not in versions:
-		versions.append(version)
 
 directories = []
-for version in versions:
-	directories.append(f"{config['download_mods_location']}/{version}")
-for instance in config['instances']:
-	directory = instance['directory']
-	if directory not in directories:
-		directories.append(directory)
+for instance in instances:
+	mods_directory = f"{instance['directory']}/mods"
+	version = instance['version']
+	loader = instance['loader'].capitalize()
 
-mods_deleted = 0
+	if f"{config['download_mods_location']}/{loader}/{version}" not in directories:
+		directories.append(f"{config['download_mods_location']}/{loader}/{version}")
+
+	if loader == "Fabric":
+		if mods_directory not in directories:
+			directories.append(mods_directory)
+
 for directory in directories:
+	log(f"	Scanning {directory}...")
 	cache = {}
 	for mod in [file for file in os.listdir(directory) if file.endswith(".jar")]:
 		path = f"{directory}/{mod}"
@@ -186,17 +212,10 @@ for directory in directories:
 		else:
 			if tmodified > cache[mod_id]['tmodified']:
 				os.remove(cache[mod_id]['path'])
-				mods_deleted += 1
-				log(f"	[INFO] Deleted {cache[mod_id]['path']}")
+				log(f"		[INFO] Deleted {os.path.basename(cache[mod_id]['path'])}")
 				cache[mod_id] = {'path': path, 'tmodified': tmodified}
 			else:
 				os.remove(path)
-				mods_deleted += 1
-				log(f"	[INFO] Deleted {path}")
+				log(f"		[INFO] Deleted {os.path.basename(path)}")
 
-if mods_deleted == 1: log(f"	Deleted {mods_deleted} file.")
-else: log(f"	Deleted {mods_deleted} files.")
-
-
-"""DONE"""
-log(f"[INFO] DONE\n")
+log(f"	Done")
