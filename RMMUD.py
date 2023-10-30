2450abe7e33bd3efdfcb8613c25e2f96b8d1f161
RandomGgames <gianopp@gmail.com>
2023-10-30 02:44:10 -0400

    Update RMMUD.py

--- RMMUD.py
+++ RMMUD.py
@@ -1,10 +1,12 @@
 import json
 import logging
 import os
+import requests
 import shutil
 import subprocess
 import sys
 import webbrowser
+import yaml
 import zipfile
 from datetime import datetime
 from importlib.util import find_spec
@@ -19,34 +21,6 @@ __version__ = '.'.join(str(x) for x in __version_info__)
 
 # sydney = <3 for gian 4 evr
 # ^^^ My girlfriend wrote this for me, I am not removing it.
-
-depends = ("requests", "pyyaml")
-
-if all(find_spec(x) is None for x in depends):
-    def call_python(context: SimpleNamespace, *py_args: str) -> None:
-        """Executes the newly created Python using safe-ish options"""
-        args: list[str] = [context.env_exec_cmd, *py_args]
-        env = os.environ.copy()
-        env['VIRTUAL_ENV'] = context.env_dir
-        env.pop('PYTHONHOME', None)
-        env.pop('PYTHONPATH', None)
-        subprocess.check_output(
-            args, env=env, cwd=context.env_dir, executable=context.env_exec_cmd)
-
-    # Make the virtual environment
-    env_dir = os.path.abspath('.venv')
-    virtualenv = EnvBuilder(with_pip=True)
-    virtualenv.create(env_dir)
-    context = virtualenv.ensure_directories(env_dir)
-
-    # Install the dependencies
-    call_python(context, '-m', 'pip', 'install', *depends)
-
-    # Run this file in the virtual environment
-    exit(subprocess.call([context.env_exec_cmd, __file__]))
-
-import requests  # noqa
-import yaml  # noqa
 
 Mods = dict[str, list[str] | dict[str, list[str]]] | list[str] | str
 Config = TypedDict("Config", {
