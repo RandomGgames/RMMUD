b9b32fe6600c5744f4c5b2a439e9c2212a847daf
RandomGgames <gianopp@gmail.com>
2023-11-04 21:21:46 -0400

    Changed logging level

--- RMMUD.py
+++ RMMUD.py
@@ -535,7 +535,7 @@ if __name__ == '__main__':
     
     # Set up logging
     logging.basicConfig(
-        level = logging.DEBUG,
+        level = logging.INFO,
         format = '%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
         datefmt = '%Y/%m/%d %H:%M:%S',
         encoding = 'utf-8',
