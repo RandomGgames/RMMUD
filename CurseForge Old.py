37044d9c5e051da60dfdd1ec1be09e6515f891a2
Gianluca O <gianopp@gmail.com>
2023-12-26 15:54:43 -0500

    Update CurseForge Old.py

--- CurseForge Old.py
+++ CurseForge Old.py
@@ -8,18 +8,6 @@ logger = logging.getLogger(__name__)
 def downloadCurseforgeMod(mod_id: str, mod_loader: str, minecraft_version: str, mod_version: str, download_dir: str, instance_dirs: list[str], curseforge_api_key: str) -> None:
         logger.info(f'Updating {mod_id} for {mod_loader} {minecraft_version}')
         
-        # Getting mod ID
-        logger.debug(f'Getting mod ID from CurseForge')
-        url = 'https://api.curseforge.com/v1/mods/search'
-        params = {'gameId': '432','slug': mod_id, 'classId': '6'}
-        curseforge_header = {'Accept': 'application/json','x-api-key': curseforge_api_key}
-        try:
-            response = requests.get(url, params, headers = curseforge_header).json()['data']
-            curseforge_mod_id = response[0]['id']
-        except Exception as e:
-            logger.warning(f'Could not fetch CurseForge ID for "{mod_id}": {repr(e)}')
-            return
-        
         # Get latest or desired mod version
         logger.debug(f'Getting files from CurseForge')
         curseforge_mod_loader = { 'forge': 1, 'fabric': 4 }.get(mod_loader, None)
